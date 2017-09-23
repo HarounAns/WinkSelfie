79d18eb61f242e289d80cc0c040d60827f5bbf48
HarounAns <hansari97@gmail.com>
2017-09-22 20:15:16 -0700

    Add files via upload

--- openCV1/main.cpp
+++ openCV1/main.cpp
@@ -1,3 +1,6 @@
+//Author: Haroun Ansari
+//Date: 4/28/2017
+
 #include "opencv2/objdetect.hpp"
 #include "opencv2/videoio.hpp"
 #include "opencv2/highgui.hpp"
@@ -9,15 +12,14 @@
 #include <fstream> 
 
 using namespace std;
-using namespace cv;
+using namespace cv; //openCV lib standard
 
-// much of this code was adapted from the opencv tutorial
 
 /** Function Headers */
 void detectAndDisplay(Mat frame);
 
 /** Global variables */
-String face_cascade_name, eyes_cascade_name;
+String face_cascade_name, eyes_cascade_name; // needed for communication between functions
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
 String window_name = "Capture - Face detection";
@@ -26,59 +28,59 @@ String window_name = "Capture - Face detection";
 int main(int argc, const char** argv)
 {
 
-	face_cascade_name = "haarcascade_frontalface_alt.xml";
-	eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
-	VideoCapture capture;
+	face_cascade_name = "haarcascade_frontalface_alt.xml"; // openCV XML values for face detection
+	eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml"; // openCV XML values for eye detection
+	VideoCapture capture; //capture object
 
-	vector<int> compression_params;
-	compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+	vector<int> compression_params; //vector data structure using C STL
+	compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION); //compression used for init data in vector
 	compression_params.push_back(9);
 
-	Mat frame;
-	int num_frames = 100;
-	time_t start, end;
+	Mat frame; //Mat obj frame
+	int num_frames = 100; //number of frames were storing in queue
+	time_t start, end; // used for timer
 
 	//-- 1. Load the cascades
-	if (!face_cascade.load(face_cascade_name)) { printf("--(!)Error loading face cascade\n"); return -1; };
+	if (!face_cascade.load(face_cascade_name)) { printf("--(!)Error loading face cascade\n"); return -1; };//error checking
 	if (!eyes_cascade.load(eyes_cascade_name)) { printf("--(!)Error loading eyes cascade\n"); return -1; };
 
 	//-- 2. Read the video stream
 	capture.open(0);
-	if (!capture.isOpened()) { printf("--(!)Error opening video capture\n"); return -1; }
+	if (!capture.isOpened()) { printf("--(!)Error opening video capture\n"); return -1; }//error checking
 
 	time(&start); // start timer
-	while (capture.read(frame))
+	while (capture.read(frame)) //occuring as long as openCV is capturing data
 	{
 		
 
-		if (frame.empty())
+		if (frame.empty()) 
 		{
 			printf(" --(!) No captured frame -- Break!");
 			break;
 		}
 
 		//-- 3. Apply the classifier to the frame
-		detectAndDisplay(frame);
+		detectAndDisplay(frame); 
 
-		char c = (char)waitKey(10);
-		if (c == 27) { 
-			imwrite("output.png", frame, compression_params);
+		char c = (char)waitKey(10); 
+		if (c == 27) { //when wink detected
+			imwrite("output.png", frame, compression_params); // output picture
 			break; } // escape
 		
 		
 		
 	}
 	
-		ofstream ofs("output.txt", ios::out | ios::app);
+		ofstream ofs("output.txt", ios::out | ios::app); // sends frame data to output file
 		time(&end); // end timer
 
 		// Time elapsed
 		double seconds = difftime(end, start);
-		ofs << "Time taken : " << seconds << " seconds" << endl;
+		ofs << "Time taken : " << seconds << " seconds" << endl; //outputs time taken
 
-		ofs << "N = " << num_frames << " frames" << endl;
+		ofs << "N = " << num_frames << " frames" << endl; //outputs frame number
 
-		ofs << "t-N+1 = " << seconds - num_frames + 1 << endl;
+		ofs << "t-N+1 = " << seconds - num_frames + 1 << endl; //end value
 
 		cout << "Output file 'output.txt' created" << endl;
 		cout << "Output picture 'output.png' created" << endl;
@@ -102,25 +104,25 @@ void detectAndDisplay(Mat frame)
 	//-- Detect faces
 	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
 
-	for (size_t i = 0; i < faces.size(); i++)
+	for (size_t i = 0; i < faces.size(); i++) //takes in face data such as size and shape
 	{
-		Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
-		ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+		Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2); //circumference, width, and height used
+		ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0); // creates an ellipse to put around face
 
-		Mat faceROI = frame_gray(faces[i]);
+		Mat faceROI = frame_gray(faces[i]); //puts ellipse over face
 		std::vector<Rect> eyes;
 
 		//-- In each face, detect eyes
-		eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+		eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30)); // take in eye data within face element
 
 		for (size_t j = 0; j < eyes.size(); j++)
 		{
-			Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
-			int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
-			circle(frame, eye_center, radius, Scalar(255, 0, 0), 4, 8, 0);
+			Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2); // eye circumference, height and width search
+			int radius = cvRound((eyes[j].width + eyes[j].height)*0.25); // uses smaller circle radius to detect eyes
+			circle(frame, eye_center, radius, Scalar(255, 0, 0), 4, 8, 0); // creates circle to surround eyes
 		}
 	}
-	while (extra_credit == true) {
+	while (extra_credit == true) { //secret of universe mode
 	}
 	imshow(window_name, frame);
 	
